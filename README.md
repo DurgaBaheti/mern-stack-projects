18537bf2fb218137a1e9a5a2b8906fca5bbd40e8
dheerajbaheti <145109215+DurgaBaheti@users.noreply.github.com>
2024-06-13 10:20:40 +0000

    unnecessary - 1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mern-stack-projects
 full stack projects
+
+# for "vite not found"-
+    run "npm i vite"
--- videoStreamingApp/index.js
+++ videoStreamingApp/index.js
@@ -5,7 +5,6 @@ import { v4 as uuidv4 } from "uuid"
 import path from "path"
 import fs from "fs"
 import {exec} from "child_process"
-import { stderr, stdout } from "process"
 
 const app = express();
 
